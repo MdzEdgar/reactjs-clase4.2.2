f0542fcff7c74bdd3b9e85e51b1b1086c3427223
Edgar Méndez <mdzredgar@gmail.com>
2023-02-09 21:05:07 -0600

    Mejora en el codigo

--- src/App.jsx
+++ src/App.jsx
@@ -1,77 +1,103 @@
-import axios from 'axios'
-import { useState, useEffect } from 'react'
-import { useForm } from 'react-hook-form'
-import './App.css'
-import TaskCard from './components/TaskCard'
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import "./App.css";
+import TaskCard from "./components/TaskCard";
 
-const BASE_URL = "https://todos-crud.academlo.tech/"
+const BASE_URL = "https://todos-crud.academlo.tech/";
 
 const defaultValues = {
   title: "",
-  description: ""
-}
+  description: "",
+};
 
 function App() {
-  const [tasks, setTasks] = useState([])
+  const [tasks, setTasks] = useState([]);
+  const [updatingTask, setUpdatingTask] = useState();
 
-  const {register, handleSubmit, reset} = useForm()
+  const { register, handleSubmit, reset } = useForm();
 
   const submit = (data) => {
-    const dataCreate = {...data, isCompleted: false}
-    createTask(dataCreate)
-    reset(defaultValues)
-  }
+    const dataCreate = { ...data, isCompleted: false };
+    if(updatingTask){
+      updateTask(updatingTask.id, data)
+      setUpdatingTask()
+    }else {
+      createTask(dataCreate);
+    }
+    reset(defaultValues);
+  };
 
   const getAllTasks = () => {
     axios
-    .get(`${BASE_URL}todos/`)
-    .then((res) => setTasks(res.data))
-    .catch((err) => console.log(err))
+      .get(`${BASE_URL}todos/`)
+      .then((res) => setTasks(res.data))
+      .catch((err) => console.log(err));
   };
 
   const createTask = (data) => {
-    axios.post(`${BASE_URL}todos/`, data)
+    axios
+      .post(`${BASE_URL}todos/`, data)
       .then(() => getAllTasks())
-      .catch((err) => console.log(err))
-  }
+      .catch((err) => console.log(err));
+  };
 
   const deleteTask = (id) => {
-    axios.delete(`${BASE_URL}todos/${id}/`)
-    .then(() => getAllTasks())
-    .catch((err) => console.log(err))
-  }
+    axios
+      .delete(`${BASE_URL}todos/${id}/`)
+      .then(() => getAllTasks())
+      .catch((err) => console.log(err));
+  };
 
-  const updateStatusTask = (id, data) => {
-    axios.patch(`${BASE_URL}todos/${id}/`, data)
-    .then(() => getAllTasks())
-    .catch((err) => console.log(err))
-  }
+  const updateTask = (id, data) => {
+    axios
+      .patch(`${BASE_URL}todos/${id}/`, data)
+      .then(() => {
+        getAllTasks();
+        console.log("Actualizada");
+      })
+      .catch((err) => console.log(err));
+  };
 
   useEffect(() => {
-    getAllTasks()
-  }, [])
+    getAllTasks();
+  }, []);
 
   return (
     <div className="App">
-      <h1>To-Do's</h1>
+      <h1>Todos</h1>
 
-      <form onSubmit={handleSubmit(submit)} >
+      <form onSubmit={handleSubmit(submit)}>
         <div>
           <label htmlFor="">Title</label>
           <input type="text" {...register("title")} />
         </div>
         <div>
           <label htmlFor="">Description</label>
-          <textarea id="" cols="30" rows="10" {...register("description")}></textarea>
+          <textarea
+            id=""
+            cols="30"
+            rows="10"
+            {...register("description")}
+          ></textarea>
         </div>
-        <button type="submit">Create</button>
+        <button type="submit">{updatingTask ? "Update" : "Create"}</button>
       </form>
 
       <section>
-        {tasks.map((task) => <TaskCard key={task.id} task={task} deleteTask={deleteTask} updateStatusTask={updateStatusTask} />)}
+        {tasks.map((task) => (
+          <TaskCard
+            key={task.id}
+            task={task}
+            deleteTask={deleteTask}
+            updateTask={updateTask}
+            setUpdatingTask={setUpdatingTask}
+            reset={reset}
+          />
+        ))}
       </section>
     </div>
   );
 }
 
-export default App
+export default App;
--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,21 +1,28 @@
 import React from 'react'
 
-const TaskCard = ({task, deleteTask, updateStatusTask}) => {
+const TaskCard = ({task, deleteTask, updateTask, setUpdatingTask, reset}) => {
 
-  const handleChangeCheckBox = (e) => {
+  const handleChangeCheckbox = (e) => {
     const data = { isCompleted: e.target.checked }
-    updateStatusTask(task.id, data)
+    updateTask(task.id, data)
+  }
+
+  const handleClickEdit = () => {
+    setUpdatingTask(task)
+    reset(task)
+    console.log(task)
   }
 
   return (
     <article>
-      <h3 className={task.isCompleted ? "complete" : ""} >{task.title}</h3>
-      <p className={task.isCompleted ? "complete" : ""} >{task.description}</p>
+      <h3 className={task.isCompleted ? "complete" : ""}>{task.title}</h3>
+      <p className={task.isCompleted ? "complete" : ""}>{task.description}</p>
       <h4>{task.isCompleted ? "Complete" : "In progress"}</h4>
       <div>
-        <input onChange={handleChangeCheckBox} type="checkbox" defaultValue={task.isCompleted} />
+        <input onChange={handleChangeCheckbox} defaultValue={task.isCompleted} type="checkbox" />
       </div>
       <button onClick={() => deleteTask(task.id)}><i className='bx bxs-trash'></i></button>
+      <button onClick={handleClickEdit}><i className='bx bx-edit'></i></button>
       <hr />
     </article>
   )
